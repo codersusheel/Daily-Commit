9fa06cf4ae4d870ede88c28d79281c5cbed3de7b
susheel kumar <techiesusheel@gmail.com>
2026-02-25 11:49:52 +0530

    .

---
+++ 2026-leccion/JS/script.js
@@ -0,0 +1,87 @@
+
+
+// ---------alert ushe ---------------------------------
+
+// alert('hello'); 
+
+// alert('hii this is my first js clsss');
+
+// alert('first js code run alert');
+
+// --------console ushe----------------------------------
+
+// console.log('hello haproven');
+// console.error('haproven page error');
+// console.warn('haproven page warn');
+
+// ------- let , const-----------------------------------
+
+// const age =30;
+//  age = 32;
+
+// let my_age;  
+//  my_age=21;
+
+//  const my_num = 9122;
+
+//  console.log(my_age);
+//  console.log(my_age);
+//  console.log(my_num);
+
+
+// --- string ,numbers, boolean ,null , undefind , symbol
+
+// const name = 'susheel';
+// const age = 21;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+
+// // console.log('my name is ' + name + 'and i am '+ age  );
+// alert('my name is ' + name + ' and i am '+ age  );
+
+// const hello =` my name is ${nme} and i am ${age}`;
+
+// conconsole.log(typeof name);
+
+ 
+//  ----length , toUpperCase ,toLowerCase .substring ,split
+
+// const s ='hello susheel';
+
+ // console.log (s.length);
+ // console.log(s.toUpperCase());
+// // console.log(s.substring(0 ,5));
+
+// alert(s.substring(0 ,5).toUpperCase());
+
+// alert(s.split(''));
+// console.log(s.split(', '));
+''
+
+
+/* multi
+line 
+comments
+example
+this
+
+*/
+
+// ----- Arrays
+
+// const num = new Array(1,2,3,4,5);
+// alert(num) ;
+
+
+// const fruits =['apple','orange','banna',10,true ];
+// alert(fruits);
+
+
+
+
+
+
+
