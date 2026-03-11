41b1f7318a3f39d79513b349932f8a4028de24fa
Susheel Kumar (susheelcoder) <techiesusheel@gmail.com>
2026-03-11 21:34:07 +0530

    Create C/first.c

---
+++ 2026-leccion/C/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x; // Variable declare kiya
+    printf("Ek number enter karein: ");
+    
+    // Input lene ke liye
+    scanf("%d", &x); 
+    
+    printf("Aapne enter kiya: %d", x);
+    return 0;
+}
