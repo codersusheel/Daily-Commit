20587e4ee5d8271bff0be7c648230907a353bdac
Susheel Kumar (susheelcoder) <techiesusheel@gmail.com>
2026-03-13 22:45:16 +0530

    Update 3.c

--- 2026-leccion/C/3.c
+++ 2026-leccion/C/3.c
@@ -25,3 +25,32 @@ int main() {
 
     return 0;
 }
+
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n, i, isPrime = 1;
+    printf("Number enter karein: ");
+    scanf("%d", &n);
+
+    if (n <= 1) isPrime = 0;
+
+    // sqrt(n) tak check karna kaafi hota hai
+    for (i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+
+    if (isPrime)
+        printf("%d Prime number hai.\n", n);
+    else
+        printf("%d Prime number nahi hai.\n", n);
+
+    return 0;
+}
+
