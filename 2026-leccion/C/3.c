ecb2d0380d5a22927a30525f2dcba3d8d0c6b515
Susheel Kumar (susheelcoder) <techiesusheel@gmail.com>
2026-03-13 22:44:10 +0530

    Create 3.c

---
+++ 2026-leccion/C/3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, flag = 0;
+    printf("Ek number enter karein: ");
+    scanf("%d", &n);
+
+    // 0 aur 1 prime numbers nahi hote
+    if (n <= 1) {
+        flag = 1;
+    }
+
+    for (i = 2; i <= n / 2; ++i) {
+        // Agar n 'i' se poora divide ho jaye
+        if (n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("%d ek Prime number hai.\n", n);
+    else
+        printf("%d Prime number nahi hai.\n", n);
+
+    return 0;
+}
